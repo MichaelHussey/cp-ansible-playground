3b1445e37d3cabde3b06cb8ec3307fc6bdaab057
Mic Hussey <mic@confluent.io>
2021-05-18 22:29:20 +0200

    Generalised the mechanism for fixing known_hosts by reading the docker-compose.yml file

--- fix_known_hosts.sh
+++ fix_known_hosts.sh
@@ -4,6 +4,13 @@
 # In order for password-less ssh login to work we need to grab their public keys
 # into the known_hosts file
 rm -f ~/.ssh/known_hosts
-ssh-keyscan ya4533.local.net >> ~/.ssh/known_hosts
-ssh-keyscan ya4534.local.net >> ~/.ssh/known_hosts
-ssh-keyscan ya4535.local.net >> ~/.ssh/known_hosts
+
+if [ "$1" ]; then
+  COMPOSE_FILE="$1"
+else
+  COMPOSE_FILE=docker-compose.yml
+fi
+cat $COMPOSE_FILE | grep hostname | grep -v "#" | grep -v "ansible" | cut -d ":" -f 2 | while read -r host ; do
+  echo "Adding $host"
+  ssh-keyscan $host >> ~/.ssh/known_hosts
+done
